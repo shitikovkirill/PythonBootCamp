36ba513d9a1b127ebbe188ca940b4df4937d1da6
Shitikov Kyrylo <kshy@isddesign.com>
2017-12-08 21:40:42 +0200

    Fix decorator prints #17

--- pbc_app/log.py
+++ pbc_app/log.py
@@ -1,7 +1,7 @@
 def arguments_log(func):
     def wrapper(*args):
         for a in args:
-            print 'fibonacci_xrange({})'.format(a)
+            print '{}({})'.format(func.func_name, a)
         return func(*args)
 
     return wrapper
