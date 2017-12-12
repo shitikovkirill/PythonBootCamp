482b5659500b70f0cc26981f9a606e93601149ad
Shitikov Kyrylo <kshy@isddesign.com>
2017-12-12 21:12:55 +0200

    Install firefox + geckodriver on VM

--- bootstrap/selenium.sh
+++ bootstrap/selenium.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 cd
-wget -O selenium-server-standalone-3.8.1.jar https://selenium-release.storage.googleapis.com/3.8/selenium-server-standalone-3.8.1.jar
+echo "Install selenium"
+wget -O selenium-server-standalone-3.8.0.jar https://goo.gl/SVuU9X
+
+echo "Install firefox + geckodriver on VM"
+add-apt-repository ppa:mozillateam/firefox-next
+apt-get -y install firefox
+wget https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz
+tar -xvzf geckodriver*
+mv geckodriver /usr/local/sbin
