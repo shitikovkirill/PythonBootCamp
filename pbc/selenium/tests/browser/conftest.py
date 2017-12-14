811570519b82f8281e6d303cfb6f9a4bdb005159
Shitikov Kyrylo <kshy@isddesign.com>
2017-12-15 00:36:32 +0200

    Merge 2 test module files in 1

--- pbc/selenium/tests/browser/conftest.py
+++ pbc/selenium/tests/browser/conftest.py
@@ -12,3 +12,13 @@ def up_grid(ssh_client):
     grid.start_hub()
     grid.add_node()
     time.sleep(10)
+
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function")
+def web_driver():
+    driver = webdriver.Firefox()
+    yield driver
+    driver.close()
--- pbc/selenium/tests/browser/page/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-
-@pytest.fixture(scope="function")
-def web_driver():
-    driver = webdriver.Firefox()
-    yield driver
-    driver.close()
