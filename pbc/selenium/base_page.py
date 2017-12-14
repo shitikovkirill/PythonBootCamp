89ead7177a454511bb67cc77387faa848d430d21
Shitikov Kyrylo <kshy@isddesign.com>
2017-12-15 00:37:42 +0200

    Add method to base file

--- pbc/selenium/base_page.py
+++ pbc/selenium/base_page.py
@@ -7,3 +7,19 @@ class BasePage:
     @abstractmethod
     def get_title(self):
         pass
+
+    @abstractmethod
+    def source(self):
+        pass
+
+    @abstractmethod
+    def screenshot(self, name):
+        pass
+
+    @abstractmethod
+    def enter_data(self, input_el_name, value, button):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
